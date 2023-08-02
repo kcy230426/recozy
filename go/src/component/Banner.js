0779b5a8ddb68571353cb003fa786a41ed251568
kcy230426 <purple_galaxy@naver.com>
2023-08-02 14:21:08 +0900

    누락된 이미지 추가

--- go/src/component/Banner.js
+++ go/src/component/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
             <div id="banner">
                 <img src={bg} alt="메인 배경" className='img-fluid' />
                 <div className="textbox">
-                    <h3>이번 주말, <span className="coloring">힐링</span> 어떠세요?</h3>
+                    <h3>이이번 주말, <span className="coloring">힐링</span> 어떠세요?</h3>
                     <p>나얼과 Recozy가 특별한 하루를 만들어 드립니다.</p>
                 </div>
             </div>
